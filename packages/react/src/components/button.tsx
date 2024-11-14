87b1f501302c3e9ec28887264dde860c0b1f5a80
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-11-14 10:53:19 -0400

    feat: button component

---
+++ packages/react/src/components/button.tsx
@@ -0,0 +1,94 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  fontFamily: '$default',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  border: 0,
+  fontWeight: '$medium',
+  color: '$white',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        backgroundColor: '$villo500',
+
+        '&:not(:disabled):hover': {
+          background: '$villo300',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$villo300',
+        border: '2px solid $villo500',
+
+        '&:not(:disabled):hover': {
+          background: '$villo500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
